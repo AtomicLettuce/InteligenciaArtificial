e0da80069e442fd34961fac81169b3f31bd1252f
Laptop Youis <lluispico922@gmail.com>
2022-11-12 18:10:23 +0100

    cd

--- practica1/agent_A_estrella.py
+++ practica1/agent_A_estrella.py
@@ -7,17 +7,46 @@ ClauPercepcio:
 """
 from ia_2022 import entorn
 from practica1 import joc
+from estat import Estat
+from queue import PriorityQueue
 from practica1.entorn import ClauPercepcio, AccionsRana, Direccio
 
-class Estat:
-    def __init__(self, info:int[], pes:int, pare=None):
-        self.__info=info
-        self.__pare=pare
-        self.__pes=pes
-    
-    def __eq__(self, other: Estat) -> bool:
-        return self.__info[0] ==other.info[0] & self.__info[1]==other.info[1]
 
-    def es_meta(self) -> bool:
-        return ClauPercepcio.OLOR[0]==self.__info[0] & ClauPercepcio.OLOR[1]==self.__info[1]
+class Rana(joc.Rana):
     
+    def __init__(self, *args, **kwargs):
+        super(Rana, self).__init__(*args, **kwargs)
+        self.__oberts=None
+        self.__tancats=None
+        self.__accions=None
+
+    def pinta(self, display):
+        pass
+
+    def actua(self, percep: entorn.Percepcio) -> entorn.Accio | tuple[entorn.Accio, object]:
+
+        estat_inicial=Estat(percep(ClauPercepcio.POSICIO),0, pare=None)
+
+        if self.__accions is None:
+            self.cerca(estat_inicial)
+        
+        if self.__accions:
+            acc=self.__accions.pop()
+        
+        return acc
+        
+        return AccionsRana.ESPERAR
+    
+    def cerca(self, estat_inicial):
+        self.__oberts = PriorityQueue()
+        self.__tancats=set()
+        self.oberts.put(estat_inicial.get_heuristica(), estat_inicial)
+
+        actual = None
+
+        while  not self.obers.empty():
+            _, actual=self.oberts.get()
+            if actual in self.tancats:
+                continue
+            if actual.es_meta()
+
