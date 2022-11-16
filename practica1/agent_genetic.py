8f2d041cc5acd196fe55241eeb9080fe58bb197e
Atomic_Lettuce <xvivesm@gmail.com>
2022-11-16 19:22:54 +0100

    inici genetic

--- practica1/agent_genetic.py
+++ practica1/agent_genetic.py
@@ -2,10 +2,11 @@ from ia_2022 import entorn
 from practica1 import joc
 from estat import Estat
 from practica1.entorn import ClauPercepcio, AccionsRana, Direccio
+from random import randint
 
 
 class RanaGenetica(joc.Rana):
-     
+
     def __init__(self, *args, **kwargs):
         super(RanaGenetica, self).__init__(*args, **kwargs)
         self.__oberts = None
@@ -15,7 +16,53 @@ class RanaGenetica(joc.Rana):
     def pinta(self, display):
         pass
 
-    def cerca(self, estat, percep):
-        self.__oberts = []
-        
+    def actua(self, percep: entorn.Percepcio) -> entorn.Accio | tuple[entorn.Accio, object]:
     
+        return AccionsRana.ESPERAR
+
+class Individu:
+    # Totes les accions que se poden fer
+    acc_pos=[AccionsRana.ESPERAR]
+    for d in Direccio:
+        ac=(AccionsRana.MOURE,d)
+        acc_pos.append(ac)
+        ac=(AccionsRana.BOTAR,d)
+        acc_pos.append(ac)
+    
+    def __init__(self, pos_inicial:tuple):
+        self.__accions=None
+        self.__pos_inicial=pos_inicial
+        self.__pos_final=pos_inicial
+    
+    def ha_arribat(self,percep)->bool:
+        return self.__pos_final[0]==percep[ClauPercepcio.OLOR][0]and self.__pos_final[1]==percep[ClauPercepcio.OLOR]
+    
+    def es_legal(self,accio, percep):
+        if accio==AccionsRana.ESPERAR:
+            return True
+        if accio[0]==AccionsRana.MOURE:
+            if accio[1]==Direccio.DRETA:
+                if self.__pos_final[0]+1>percep[ClauPercepcio.MIDA_TAULELL][0] or (self.__pos_final[0]+1,self.__pos_final[1]) in ClauPercepcio.PARETS:
+                    return False
+            if accio[1]==Direccio.ESQUERRE:
+                pass
+            if accio[1]==Direccio.DALT:
+                pass
+            if accio[1]==Direccio.BAIX:
+                pass
+
+
+
+        pass
+
+
+
+    #indica quants de moviments vol generar per aquest individu
+    def genera_moviments(self, qt:int, percep):
+        self.__accions=[]
+
+
+        for i in range(qt):
+            a=self.acc_pos[randint(0,len(self.acc_pos))]
+            if es_legal(a,percep):
+                self.__accions.append(a)
